65182e3b37e91b8b3b5e8117f7c8878ec6130838
maiga <maigalex91@gmail.com>
2023-02-13 23:57:58 +0100

    ajout commentaires

--- src/main/java/com/maigrot/springfoot/controller/PlayerController.java
+++ src/main/java/com/maigrot/springfoot/controller/PlayerController.java
@@ -10,18 +10,33 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-
+/**
+ * Controller pour les joueurs
+ */
 @RestController
 @RequestMapping("/api/players")
 public class PlayerController {
+
+    // Référence au repository des joueurs
     @Autowired
     private PlayerRepository playerRepository;
 
+
+    /**
+     * Récupère la liste de tous les joueurs
+     * @return la liste de tous les joueurs
+     */
     @GetMapping("")
     public List<Player> getAllPlayers() {
         return playerRepository.findAll();
     }
 
+
+    /**
+     * Récupère un joueur par son ID
+     * @param playerId l'ID du joueur à récupérer
+     * @return le joueur correspondant à l'ID
+     */
     @GetMapping("/{id}")
     public ResponseEntity<Player> getPlayerById(@PathVariable(value = "id") Long playerId) {
         Optional<Player> player = playerRepository.findById(playerId);
@@ -32,16 +47,33 @@ public class PlayerController {
         }
     }
 
+    /**
+     * Ajoute un joueur dans la base de données
+     * @param player le joueur à ajouter
+     * @return le joueur ajouté
+     */
     @PostMapping("")
     public Player createPlayer(@RequestBody Player player) {
         return playerRepository.save(player);
     }
 
+
+    /**
+     * Récupère la liste des joueurs d'une équipe donnée
+     * @param teamId l'ID de l'équipe
+     * @return la liste des joueurs de l'équipe
+     */
     @GetMapping("/teams/{teamId}")
     public List<Player> getPlayersByTeamId(@PathVariable(value = "teamId") Long teamId) {
         return playerRepository.findByTeamId(teamId);
     }
 
+
+    /**
+     * Supprime un joueur de la base de données
+     * @param playerId l'ID du joueur à supprimer
+     * @return une réponse vide avec un status HTTP 204 (No Content)
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletePlayer(@PathVariable(value = "id") Long playerId) {
         Optional<Player> player = playerRepository.findById(playerId);
--- src/main/java/com/maigrot/springfoot/controller/TeamController.java
+++ src/main/java/com/maigrot/springfoot/controller/TeamController.java
@@ -9,17 +9,35 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Controlleur REST pour les équipes
+ */
 @RestController
 @RequestMapping("/api/teams")
 public class TeamController {
+
+    /**
+     * Dépendance vers la source de données des équipes
+     */
     @Autowired
     private TeamRepository teamRepository;
 
+
+    /**
+     * Récupère toutes les équipes
+     * @return Liste des équipes
+     */
     @GetMapping("")
     public List<Team> getAllTeams() {
         return teamRepository.findAll();
     }
 
+
+    /**
+     * Récupère une équipe par son identifiant
+     * @param teamId Identifiant de l'équipe
+     * @return Équipe correspondante ou 404 si introuvable
+     */
     @GetMapping("/{id}")
     public ResponseEntity<Team> getTeamById(@PathVariable(value = "id") Long teamId) {
         Optional<Team> team = teamRepository.findById(teamId);
@@ -30,11 +48,22 @@ public class TeamController {
         }
     }
 
+    /**
+     * Crée une nouvelle équipe
+     * @param team Équipe à créer
+     * @return Équipe créée
+     */
     @PostMapping("")
     public Team createTeam(@RequestBody Team team) {
         return teamRepository.save(team);
     }
 
+
+    /**
+     * Supprime une équipe
+     * @param teamId Identifiant de l'équipe à supprimer
+     * @return 204 si suppression réussie, 404 sinon
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteTeam(@PathVariable(value = "id") Long teamId) {
         Optional<Team> team = teamRepository.findById(teamId);
